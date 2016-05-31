1fb6a3ed7df448aefbf246299405da11b2bcbfa6
Kevin Lin <thekevinlin@utexas.edu>
2016-05-30 22:45:43 -0500

    Fix AM/PM time bug

--- utils.js
+++ utils.js
@@ -837,7 +837,7 @@ Date.prototype.customFormat = function(formatString){
         hh = h<10?('0'+h):h;
     }
 
-    AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+    AMPM=(ampm=h<12?'AM':'PM');
     mm=(m=this.getMinutes())<10?('0'+m):m;
     ss=(s=this.getSeconds())<10?('0'+s):s;
     return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
