a2d03a3f31cbb0673291fcc3283a06fef7c3fd82
joaomgcd <support@joaoapps.com>
2016-09-02 12:20:36 +0100

    Made chrome notification subtext the name of the sender if it has no value

--- js/chromenotification.js
+++ js/chromenotification.js
@@ -1,5 +1,8 @@
 
 var back = chrome.extension.getBackgroundPage();
+var getDevices = function(){
+   return back.devices;
+}
 var ChromeNotification = function(notificationFromGcm){
 	var me = this;
 	if(notificationFromGcm){
@@ -20,90 +23,97 @@ var ChromeNotification = function(notificationFromGcm){
 		{
 			timeout = timeoutFromSettings * 1000;
 		}
-			var imagesToDownload = [];
-			imagesToDownload.push({"url":getDriveUrlFromFileId(this.notificationIcon ? this.notificationIcon : this.appIcon)});
-			imagesToDownload.push({"url":getDriveUrlFromFileId(this.image)});
+		var imagesToDownload = [];
+		imagesToDownload.push({"url":getDriveUrlFromFileId(this.notificationIcon ? this.notificationIcon : this.appIcon)});
+		imagesToDownload.push({"url":getDriveUrlFromFileId(this.image)});
+		if(me.buttons){
+			for (var i = 0; i < this.buttons.length; i++) {
+				var button = this.buttons[i];
+				imagesToDownload.push({"url":getDriveUrlFromFileId(button.icon)});
+			};
+		}
+
+		var device = getDevices().first(function(device){
+			return device.deviceId == me.senderId;
+		});			
+		doGetBase64Images(imagesToDownload,function(results){
+			var notificationIcon = getIcon(results[0]);
+			if(notificationIcon == "icons/notificationbutton.png" && me.iconData){
+				notificationIcon = ICON_DATA_PREFIX + me.iconData;
+			}
+			var image = getIcon(results[1]);
+			var notificationButtons = [];
+			if(me.replyId){
+				notificationButtons.push({"title":Constants.REPLY_DIRECTLY,"iconUrl":"icons/reply.png"});
+			}
 			if(me.buttons){
-				for (var i = 0; i < this.buttons.length; i++) {
-					var button = this.buttons[i];
-					imagesToDownload.push({"url":getDriveUrlFromFileId(button.icon)});
+				for (var i = 0; i < me.buttons.length; i++) {
+					var button = me.buttons[i];
+					notificationButtons.push({"title":button.text,"iconUrl":getIcon(results[i+2])});
 				};
-			}			
-			doGetBase64Images(imagesToDownload,function(results){
-				var notificationIcon = getIcon(results[0]);
-				if(notificationIcon == "icons/notificationbutton.png" && me.iconData){
-					notificationIcon = ICON_DATA_PREFIX + me.iconData;
-				}
-				var image = getIcon(results[1]);
-				var notificationButtons = [];
-				if(me.replyId){
-					notificationButtons.push({"title":Constants.REPLY_DIRECTLY,"iconUrl":"icons/reply.png"});
-				}
-				if(me.buttons){
-					for (var i = 0; i < me.buttons.length; i++) {
-						var button = me.buttons[i];
-						notificationButtons.push({"title":button.text,"iconUrl":getIcon(results[i+2])});
-					};
 			}
 			var text = me.text;
 			if(back.getHideNotificationText()){
 				text = "Content hidden";
 			}
-				var options = {
-						"type":"basic",
-						"iconUrl":notificationIcon,
-						"title": me.title,
-						"message": text,
-						"contextMessage": me.subText,
-						"buttons": notificationButtons,
-						"eventTime": Date.now(),
-						"requireInteraction": requireInteraction
-				};
-				if(me.image){
-					options.type = "image";
-					options.imageUrl = image;
-				}
-				if(me.priority){
-					if(me.priority < 0){
-						me.priority = 0;
-					}
-					options.priority = me.priority;
-				}
-						var appPage = getNotificationPage(me);
-				if(me.actionId || appPage){
-					options.isClickable = true;
-				}else{
-					options.isClickable = false;
-				}
-				try{
-					chrome.notifications.create(me.id, options,function(){});	
-				}catch(error){
-					delete options.requireInteraction;
-					chrome.notifications.create(me.id, options,function(){});	
-				}
-				if(back.getPlayNotificationSound()){
-					var notificationSound = back.getNotificationSound();
-					if(!notificationSound){
-						notificationSound = "resources/notification.mp3";
-					}
-					new Audio(notificationSound).play();
+			if(device && !me.subText){
+				me.subText = "From " + device.deviceName;
 			}
-				if(timeout && !requireInteraction){
-					if(timeout > 8000){
-						options.priority = 2;
-					}
-					var storedTimeout = timeouts[me.id];
-					if(storedTimeout){
-						delete timeouts[me.id];
-						clearTimeout(storedTimeout);
-					}
-					var timeoutFunc = setTimeout(function(){
-							chrome.notifications.clear(me.id, function(){});
-						delete timeouts[me.id];
-					},timeout);
-					timeouts[me.id] = timeoutFunc;
+			var options = {
+					"type":"basic",
+					"iconUrl":notificationIcon,
+					"title": me.title,
+					"message": text,
+					"contextMessage": me.subText,
+					"buttons": notificationButtons,
+					"eventTime": Date.now(),
+					"requireInteraction": requireInteraction
+			};
+			if(me.image){
+				options.type = "image";
+				options.imageUrl = image;
+			}
+			if(me.priority){
+				if(me.priority < 0){
+					me.priority = 0;
 				}
-			})
+				options.priority = me.priority;
+			}
+					var appPage = getNotificationPage(me);
+			if(me.actionId || appPage){
+				options.isClickable = true;
+			}else{
+				options.isClickable = false;
+			}
+			try{
+				chrome.notifications.create(me.id, options,function(){});	
+			}catch(error){
+				delete options.requireInteraction;
+				chrome.notifications.create(me.id, options,function(){});	
+			}
+			if(back.getPlayNotificationSound()){
+				var notificationSound = back.getNotificationSound();
+				if(!notificationSound){
+					notificationSound = "resources/notification.mp3";
+				}
+				new Audio(notificationSound).play();
+			}
+			if(timeout && !requireInteraction){
+				if(timeout > 8000){
+					options.priority = 2;
+				}
+				var storedTimeout = timeouts[me.id];
+				if(storedTimeout){
+					delete timeouts[me.id];
+					clearTimeout(storedTimeout);
+				}
+				var timeoutFunc = setTimeout(function(){
+						chrome.notifications.clear(me.id, function(){});
+					delete timeouts[me.id];
+				},timeout);
+				timeouts[me.id] = timeoutFunc;
+			}
+		})
 
 	}
 }
