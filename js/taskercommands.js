85e1e5a86250124117bfafabcd4cd34ba3b201d4
joaomgcd <joaomgcd@gmail.com>
2017-12-19 14:09:26 +0000

    Some changes for Custom Commands Layout

--- js/taskercommands.js
+++ js/taskercommands.js
@@ -73,12 +73,13 @@ var TaskerCommandsUI = function(taskerCommandsTab){
 	var divCommands =  document.createElement('div');
 	divCommands.classList.add("taskerCommands");	
 	var htmlCommand = `
-		<h2 class="taskerCommandTitle"></h2>
-		<div class="taskerCommandTop">
+		<div class="taskerCommandTitle">
 			<div class="taskerCommandIcon"><img id="commandicon" /></div>			
 			<div class="taskerCommandName" ><input type="text" placeholder="Name" /></input></div>
-			<div class="taskerCommandText" ><input type="text" placeholder="Command"  /></div>
 			<div class="taskerCommandDelete"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg></div>
+		</div>
+		<div class="taskerCommandTop">
+			<div class="taskerCommandText" ><input type="text" placeholder="Command"  /></div>
 		</div>		
 		<div class="taskerCommandPrompt" ><input type="text" placeholder="Prompt Text: if set will use command text above as a prefix and prompt for the suffix when ran"  /></div>
 		<div class="devicesToApply"></div>
@@ -140,14 +141,14 @@ var TaskerCommandsUI = function(taskerCommandsTab){
 			var deleteElement = divCommand.querySelector(".taskerCommandDelete");
 			var titleElement = divCommand.querySelector(".taskerCommandTitle");
 
-			if(command.label) {nameElement.setAttribute("value",command.label);titleElement.innerHTML = command.label;}
+			if(command.label) {nameElement.setAttribute("value",command.label);}
 			if(command.commandText) textElement.setAttribute("value",command.commandText);
 			if(command.promptText) promptTextElement.value = command.promptText;
 			nameElement.onkeyup = e => {
 				var commandElement = findCommandElement(e);
-				var titleElement = commandElement.querySelector(".taskerCommandTitle");
+				//var titleElement = commandElement.querySelector(".taskerCommandTitle");
 				commandElement.command.label = e.target.value;
-				titleElement.innerHTML = e.target.value;
+				//titleElement.innerHTML = e.target.value;
 				saveDelayed();
 			}
 			textElement.onkeyup = e => {
